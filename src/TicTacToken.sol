a4c2625a7d8d2f2f8c37b40498301b1033bc5fb2
Nick Dyer <nickbdyer@users.noreply.github.com>
2021-11-18 21:30:23 +0000

    Add game winner, turn logic and validations

--- src/TicTacToken.sol
+++ src/TicTacToken.sol
@@ -2,27 +2,85 @@
 pragma solidity ^0.8.0;
 
 contract TicTacToken {
-    string[9] public board;
+    uint256[9] public board;
+    
+    uint256 internal turns;
+    uint256 internal constant X = 1;
+    uint256 internal constant O = 2; 
 
-    function getBoard() public view returns (string[9] memory) {
+    function getBoard() public view returns (uint256[9] memory) {
         return board;
     }
 
-    function markSpace(uint256 space, string calldata symbol) public {
+    function markSpace(uint256 space, uint256 symbol) public {
+        require(_validTurn(symbol), "Not your turn");
+        require(_validSpace(space), "Invalid space");
         require(_validSymbol(symbol), "Invalid symbol");
         require(_emptySpace(space), "Already marked");
+        turns++;
         board[space] = symbol;
     }
 
+    function _validTurn(uint256 symbol) internal view returns (bool) {
+        return currentTurn() == symbol;
+    }
+
+    function _validSpace(uint256 i) internal pure returns (bool) {
+        return i < 9;
+    }
+
     function _emptySpace(uint256 i) internal view returns (bool) {
-        return _compareString(board[i], "");
+        return board[i] == 0;
     }
 
-    function _validSymbol(string memory symbol) internal pure returns (bool) {
-        return _compareString(symbol, "X") || _compareString(symbol, "O");
+    function _validSymbol(uint256 symbol) internal pure returns (bool) {
+        return symbol == X || symbol == O;
     }
 
-    function _compareString(string memory a, string memory b) internal pure returns (bool) {
-        return keccak256(abi.encodePacked(a)) == keccak256(abi.encodePacked(b));
+    function currentTurn() public view returns (uint256) {
+        return (turns % 2 == 0) ? X : O;
+    }
+
+    function winner() public view returns (uint256) {
+        return _checkWins();
+    }
+
+    function _checkWins() internal view returns (uint256) {
+        uint256[8] memory wins = [
+            _row(0),
+            _row(1),
+            _row(2),
+            _col(0),
+            _col(1),
+            _col(2),
+            _diag(),
+            _antiDiag()
+        ];
+        for (uint256 i = 0; i < wins.length; i++) {
+            if (wins[i] == 1) {
+                return X;
+            } else if (wins[i] == 8) {
+                return O;
+            }
+        }
+        return 0;
+    }
+
+    function _row(uint256 row) internal view returns (uint256) {
+        require(row <= 2, "Invalid row");
+        return board[row] * board[row + 1] * board[row + 2];
+    }
+
+    function _col(uint256 col) internal view returns (uint256) {
+        require(col <= 2, "Invalid col");
+        return board[col] * board[col + 3] * board[col + 6];
+    }
+
+    function _diag() internal view returns (uint256) {
+        return board[0] * board[4] * board[8];
+    }
+
+    function _antiDiag() internal view returns (uint256) {
+        return board[2] * board[4] * board[6];
     }
 }
--- src/test/TicTacToken.t.sol
+++ src/test/TicTacToken.t.sol
@@ -4,38 +4,117 @@ pragma solidity ^0.8.0;
 import "./utils/TicTacTokenTest.sol";
 
 contract TestTTT is TicTacTokenTest {
+    uint256 internal constant EMPTY = 0;
+    uint256 internal constant X = 1;
+    uint256 internal constant O = 2;
 
     function test_has_empty_board() public {
         for (uint256 i=0; i<9; i++) {
-            assertEq(ttt.board(i), "");
+            assertEq(ttt.board(i), 0);
         }
     }
 
     function test_get_board() public {
-        string[9] memory expected = ["", "", "", "", "", "", "", "", ""];
-        string[9] memory actual = ttt.getBoard();
+        uint256[9] memory expected = [
+            EMPTY,
+            EMPTY,
+            EMPTY,
+            EMPTY,
+            EMPTY,
+            EMPTY,
+            EMPTY,
+            EMPTY,
+            EMPTY
+        ];
+        uint256[9] memory actual = ttt.getBoard();
         for (uint256 i=0; i<9; i++) {
             assertEq(actual[i], expected[i]);
         }
     }
 
     function test_can_mark_space_with_X() public {
-        ttt.markSpace(0, "X");
-        assertEq(ttt.board(0), "X");
+        ttt.markSpace(0, X);
+        assertEq(ttt.board(0), X);
     }
 
     function test_can_mark_space_with_O() public {
-        ttt.markSpace(0, "O");
-        assertEq(ttt.board(0), "O");
+        ttt.markSpace(1, X);
+        ttt.markSpace(0, O);
+        assertEq(ttt.board(0), O);
     }
 
     function testFail_cannot_mark_space_with_Z() public {
-        ttt.markSpace(0, "Z");
+        ttt.markSpace(0, 3);
     }
 
     function testFail_cannot_overwrite_marked_space() public {
-        ttt.markSpace(0, "X");
-        ttt.markSpace(0, "O");
+        ttt.markSpace(0, X);
+        ttt.markSpace(0, O);
     }
 
+    function test_checks_for_horizontal_win() public {
+        ttt.markSpace(0, X);
+        ttt.markSpace(3, O);
+        ttt.markSpace(1, X);
+        ttt.markSpace(4, O);
+        ttt.markSpace(2, X);
+        assertEq(ttt.winner(), X);
+    }
+
+    function test_checks_for_vertical_win() public {
+        ttt.markSpace(1, X);
+        ttt.markSpace(0, O);
+        ttt.markSpace(2, X);
+        ttt.markSpace(3, O);
+        ttt.markSpace(4, X);
+        ttt.markSpace(6, O);
+        assertEq(ttt.winner(), O);
+    }
+
+    function test_checks_for_diagonal_win() public {
+        ttt.markSpace(0, X);
+        ttt.markSpace(1, O);
+        ttt.markSpace(4, X);
+        ttt.markSpace(5, O);
+        ttt.markSpace(8, X);
+        assertEq(ttt.winner(), X);
+    }
+
+    function test_checks_for_antidiagonal_win() public {
+        ttt.markSpace(1, X);
+        ttt.markSpace(2, O);
+        ttt.markSpace(3, X);
+        ttt.markSpace(4, O);
+        ttt.markSpace(5, X);
+        ttt.markSpace(6, O);
+        assertEq(ttt.winner(), O);
+    }
+
+    function test_returns_zero_on_no_winner() public {
+        ttt.markSpace(1, X);
+        ttt.markSpace(4, O);
+        assertEq(ttt.winner(), 0);
+    }
+
+    function test_returns_zero_on_empty_board() public {
+        assertEq(ttt.winner(), 0);
+    }
+
+    function testFail_checks_valid_space() public {
+        ttt.markSpace(10, X);
+    }
+
+    function test_tracks_current_turn() public {
+        assertEq(ttt.currentTurn(), X);
+        ttt.markSpace(1, X);
+        assertEq(ttt.currentTurn(), O);
+        ttt.markSpace(2, O);
+        assertEq(ttt.currentTurn(), X);
+    }
+
+    function testFail_cannot_mark_same_symbol_twice() public {
+        assertEq(ttt.currentTurn(), X);
+        ttt.markSpace(1, X);
+        ttt.markSpace(2, X);
+    }
 }
